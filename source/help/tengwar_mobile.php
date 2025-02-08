d780b2f20f0c1c9c9e1f5f3bf4772ae266e7c4a3
3ubaidUrRe7man <3ubaidUrRe7man@protonmail.com>
2025-02-08 20:27:35 +0500

    first commit

---
+++ source/help/tengwar_mobile.php
@@ -0,0 +1,27 @@
+<?php
+    $pagename = 'Tengwar for Mobile Keyboard Help';
+    $pagetitle = $pagename;
+    require_once('header.php');
+?>
+
+<p>
+    Tengwar for Mobile
+</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id="osk" data-states='default shift rightalt rightalt-shift'>
+    <img src="keyboard_layout.png" alt="Keyboard Layout" srcset=""><br>
+    <img src="keyboard_layout_shifted.png" alt="Shift Keyboard Layout" srcset=""><br>
+    <img src="keyboard_layout_alt.png" alt="" srcset="Alt Keyboard Layout"><br>
+    <img src="keyboard_layout_alt-shift.png" alt="Alt-Shift Keyboard Layout" srcset="">
+    <p>
+        This keyboard follows Dvorak for Programmers keyboard layout.
+    </p>
+</div>
+
+<h2>Mobile Keyboard Layout</h2>
+<div id='osk-phone' data-states='default numbers'>
+    <p>
+        This keyboard uses the same layout as the desktop layout. While characters in desktop shift, alt and alt-shift are available at long press. 
+    </p>
+</div>
